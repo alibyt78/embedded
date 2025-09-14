67edffeeb322e14d90e7151fcc1965a648e93a55
alibyt78 <53123987+alibyt78@users.noreply.github.com>
2025-09-14 23:55:58 +0330

    ESP32 with DHT Sensor
    Explains the reading of DHT temperature and humidity sensors using ESP32 and displaying data via serial monitor

---
+++ esp32_dht_sensor_read.ino
@@ -0,0 +1,24 @@
+
+#include <WiFi.h>
+#include "DHT.h"
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Serial.printf("Humidity: %.2f %%, Temp: %.2f C
+", h, t);
+  delay(2000);
+}
