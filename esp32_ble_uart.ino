a054d37589b7b8dbf13897c792400ba1427b419b
alibyt78 <53123987+alibyt78@users.noreply.github.com>
2025-09-15 20:00:17 +0330

    ESP32 BLE UART Example
    Demonstrates how to create a basic UART communication over Bluetooth Low Energy (BLE) on the ESP32 platform

---
+++ esp32_ble_uart.ino
@@ -0,0 +1,30 @@
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+BLEServer* pServer;
+BLECharacteristic* pCharacteristic;
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+void setup() {
+  Serial.begin(115200);
+  BLEDevice::init("ESP32-BLE");
+  pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                    CHARACTERISTIC_UUID,
+                    BLECharacteristic::PROPERTY_READ |
+                    BLECharacteristic::PROPERTY_WRITE
+                  );
+  pCharacteristic->setValue("Hello World");
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->start();
+}
+
+void loop() {
+}
